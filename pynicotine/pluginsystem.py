6186d4f618cea9c599cc73c4abccb5e3d88181f1
Mat <mail@mathias.is>
2020-08-14 19:32:04 +0300

    Plugin system: don't start new thread for every incoming event

--- pynicotine/pluginsystem.py
+++ pynicotine/pluginsystem.py
@@ -354,6 +354,361 @@ class PluginHandler(object):
     def appendqueue(self, item):
         # We cannot do a test after adding the item since it's possible
         # this function will be called twice simultaneously - and then
+        # len(self.guiqueue) might be 2 for both calls.# COPYRIGHT (C) 2020 Nicotine+ Team
+# COPYRIGHT (C) 2016-2017 Michael Labouebe <gfarmerfr@free.fr>
+# COPYRIGHT (C) 2016 Mutnick <muhing@yahoo.com>
+# COPYRIGHT (C) 2008-2011 Quinox <quinox@users.sf.net>
+# COPYRIGHT (C) 2009 Daelstorm <daelstorm@gmail.com>
+#
+# GNU GENERAL PUBLIC LICENSE
+#    Version 3, 29 June 2007
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import sys
+from ast import literal_eval
+from gettext import gettext as _
+from importlib.machinery import SourceFileLoader
+from time import time
+from traceback import extract_stack
+from traceback import extract_tb
+from traceback import format_list
+from traceback import print_exc
+
+from gi.repository import GLib
+
+from pynicotine.logfacility import log
+from pynicotine.pynicotine import slskmessages
+
+
+WIN32 = sys.platform.startswith("win")
+
+
+returncode = {
+    'break': 0,  # don't give other plugins the event, do let n+ process it
+    'zap': 1,    # don't give other plugins the event, don't let n+ process it
+    'pass': 2    # do give other plugins the event, do let n+ process it
+}                # returning nothing is the same as 'pass'
+
+tupletype = type(('', ''))
+
+
+class InvalidPluginError(Exception):
+    pass
+
+
+class PluginHandler(object):
+
+    frame = None  # static variable... but should it be?
+    guiqueue = []  # fifo isn't supported by older python
+
+    def __init__(self, frame, plugindir):
+        self.frame = frame
+        log.add(_("Loading plugin handler"))
+        self.myUsername = self.frame.np.config.sections["server"]["login"]
+        self.plugindirs = []
+        self.enabled_plugins = {}
+        self.loaded_plugins = {}
+        self.type2cast = {
+            'integer': int,
+            'int': int,
+            'float': float,
+            'string': str,
+            'str': str
+        }
+
+        try:
+            os.makedirs(plugindir)
+        except Exception:
+            pass
+
+        # Load system-wide plugins
+        prefix = os.path.dirname(__file__)
+        self.plugindirs.append(os.path.join(prefix, "plugins"))
+
+        # Load home directory plugins
+        self.plugindirs.append(plugindir)
+
+        if os.path.isdir(plugindir):
+            # self.load_directory(self.plugindir)
+            self.load_enabled()
+        else:
+            log.add(_("It appears '%s' is not a directory, not loading plugins.") % plugindir)
+
+    def __findplugin(self, pluginname):
+        for directory in self.plugindirs:
+            fullpath = os.path.join(directory, pluginname)
+            if os.path.exists(fullpath):
+                return fullpath
+        return None
+
+    def toggle_plugin(self, pluginname):
+        on = pluginname in self.enabled_plugins
+        if on:
+            self.disable_plugin(pluginname)
+        else:
+            self.enable_plugin(pluginname)
+
+    def load_plugin(self, pluginname):
+        path = self.__findplugin(pluginname)
+        if path is None:
+            log.add(_("Failed to load plugin '%s', could not find it.") % pluginname)
+            return False
+        sys.path.insert(0, path)
+        plugin = SourceFileLoader(pluginname, os.path.join(path, '__init__.py')).load_module()
+        instance = plugin.Plugin(self)
+        self.plugin_settings(instance)
+        instance.LoadNotification()
+        # log.add("Loaded plugin %s (version %s) from %s" % (instance.__name__, instance.__version__, modulename))
+        # self.plugins.append((module, instance))
+        sys.path = sys.path[1:]
+        self.loaded_plugins[pluginname] = plugin
+        return plugin
+
+    def enable_plugin(self, pluginname):
+        if pluginname in self.enabled_plugins:
+            return
+        try:
+            plugin = self.load_plugin(pluginname)
+            if not plugin:
+                raise Exception("Error loading plugin '%s'" % pluginname)
+            plugin.enable(self)
+            self.enabled_plugins[pluginname] = plugin
+            log.add(_("Enabled plugin %s") % plugin.PLUGIN.__name__)
+        except Exception:
+            print_exc()
+            log.addwarning(_("Unable to enable plugin %s") % pluginname)
+            # common.log_exception(logger)
+            return False
+        return True
+
+    def list_installed_plugins(self):
+        pluginlist = []
+        for dir in self.plugindirs:
+            if os.path.exists(dir):
+                for file in os.listdir(dir):
+                    if file not in pluginlist and os.path.isdir(os.path.join(dir, file)):
+                        pluginlist.append(file)
+        return pluginlist
+
+    def disable_plugin(self, pluginname):
+        if pluginname not in self.enabled_plugins:
+            return
+        try:
+            plugin = self.enabled_plugins[pluginname]
+            log.add(_("Disabled plugin {}".format(plugin.PLUGIN.__name__)))
+            del self.enabled_plugins[pluginname]
+            plugin.disable(self)
+        except Exception:
+            print_exc()
+            log.addwarning(_("Unable to fully disable plugin %s") % pluginname)
+            # common.log_exception(logger)
+            return False
+        return True
+
+    def get_plugin_settings(self, pluginname):
+        if pluginname in self.enabled_plugins:
+            plugin = self.enabled_plugins[pluginname]
+            if hasattr(plugin.PLUGIN, "metasettings"):
+                return plugin.PLUGIN.metasettings
+
+    def get_plugin_info(self, pluginname):
+        path = os.path.join(self.__findplugin(pluginname), 'PLUGININFO')
+        with open(path) as f:
+            infodict = {}
+            for line in f:
+                try:
+                    key, val = line.split("=", 1)
+                    infodict[key] = literal_eval(val)
+                except ValueError:
+                    pass  # this happens on blank lines
+        return infodict
+
+    def save_enabled(self):
+        self.frame.np.config.sections["plugins"]["enabled"] = list(self.enabled_plugins.keys())
+
+    def load_enabled(self):
+        enable = self.frame.np.config.sections["plugins"]["enable"]
+        if not enable:
+            return
+        to_enable = self.frame.np.config.sections["plugins"]["enabled"]
+        for plugin in to_enable:
+            self.enable_plugin(plugin)
+
+    def plugin_settings(self, plugin):
+        try:
+            if not hasattr(plugin, "settings"):
+                return
+            if plugin.__id__ not in self.frame.np.config.sections["plugins"]:
+                self.frame.np.config.sections["plugins"][plugin.__id__] = plugin.settings
+            for i in plugin.settings:
+                if i not in self.frame.np.config.sections["plugins"][plugin.__id__]:
+                    self.frame.np.config.sections["plugins"][plugin.__id__][i] = plugin.settings[i]
+            customsettings = self.frame.np.config.sections["plugins"][plugin.__id__]
+
+            for key in customsettings:
+                if key in plugin.settings:
+                    plugin.settings[key] = customsettings[key]
+
+                else:
+                    log.add(_("Stored setting '%(name)s' is no longer present in the plugin") % {'name': key})
+        except KeyError:
+            log.add("No custom settings found for %s" % (plugin.__name__,))
+            pass
+
+    def TriggerPublicCommandEvent(self, room, command, args):
+        return self._TriggerCommand("plugin.PLUGIN.PublicCommandEvent", command, room, args)
+
+    def TriggerPrivateCommandEvent(self, user, command, args):
+        return self._TriggerCommand("plugin.PLUGIN.PrivateCommandEvent", command, user, args)
+
+    def _TriggerCommand(self, strfunc, command, source, args):
+        for module, plugin in list(self.enabled_plugins.items()):
+            try:
+                if plugin.PLUGIN is None:
+                    continue
+                func = eval(strfunc)
+                ret = func(command, source, args)
+                if ret is not None:
+                    if ret == returncode['zap']:
+                        return True
+                    elif ret == returncode['pass']:
+                        pass
+                    else:
+                        log.add(_("Plugin %(module)s returned something weird, '%(value)s', ignoring") % {'module': module, 'value': str(ret)})
+            except Exception:
+                log.add(_("Plugin %(module)s failed with error %(errortype)s: %(error)s.\nTrace: %(trace)s\nProblem area:%(area)s") % {
+                    'module': module,
+                    'errortype': sys.exc_info()[0],
+                    'error': sys.exc_info()[1],
+                    'trace': ''.join(format_list(extract_stack())),
+                    'area': ''.join(format_list(extract_tb(sys.exc_info()[2])))
+                })
+        return False
+
+    def TriggerEvent(self, function, args):
+        """Triggers an event for the plugins. Since events and notifications
+        are precisely the same except for how n+ responds to them, both can be
+        triggered by this function."""
+        hotpotato = args
+        for module, plugin in list(self.enabled_plugins.items()):
+            try:
+                func = eval("plugin.PLUGIN." + function)
+                ret = func(*hotpotato)
+                if ret is not None and type(ret) != tupletype:
+                    if ret == returncode['zap']:
+                        return None
+                    elif ret == returncode['break']:
+                        return hotpotato
+                    elif ret == returncode['pass']:
+                        pass
+                    else:
+                        log.add(_("Plugin %(module)s returned something weird, '%(value)s', ignoring") % {'module': module, 'value': ret})
+                if ret is not None:
+                    hotpotato = ret
+            except Exception:
+                log.add(_("Plugin %(module)s failed with error %(errortype)s: %(error)s.\nTrace: %(trace)s\nProblem area:%(area)s") % {
+                    'module': module,
+                    'errortype': sys.exc_info()[0],
+                    'error': sys.exc_info()[1],
+                    'trace': ''.join(format_list(extract_stack())),
+                    'area': ''.join(format_list(extract_tb(sys.exc_info()[2])))
+                })
+        return hotpotato
+
+    def SearchRequestNotification(self, searchterm, user, searchid):
+        self.TriggerEvent("SearchRequestNotification", (searchterm, user, searchid))
+
+    def DistribSearchNotification(self, searchterm, user, searchid):
+        self.TriggerEvent("DistribSearchNotification", (searchterm, user, searchid))
+
+    def PublicRoomMessageNotification(self, room, user, line):
+        self.TriggerEvent("PublicRoomMessageNotification", (room, user, line))
+
+    def IncomingPrivateChatEvent(self, user, line):
+        if user != self.myUsername:
+            # dont trigger the scripts on our own talking - we've got "Outgoing" for that
+            return self.TriggerEvent("IncomingPrivateChatEvent", (user, line))
+        else:
+            return (user, line)
+
+    def IncomingPrivateChatNotification(self, user, line):
+        self.TriggerEvent("IncomingPrivateChatNotification", (user, line))
+
+    def IncomingPublicChatEvent(self, room, user, line):
+        return self.TriggerEvent("IncomingPublicChatEvent", (room, user, line))
+
+    def IncomingPublicChatNotification(self, room, user, line):
+        self.TriggerEvent("IncomingPublicChatNotification", (room, user, line))
+
+    def OutgoingPrivateChatEvent(self, user, line):
+        if line is not None:
+            # if line is None nobody actually said anything
+            return self.TriggerEvent("OutgoingPrivateChatEvent", (user, line))
+        else:
+            return (user, line)
+
+    def OutgoingPrivateChatNotification(self, user, line):
+        self.TriggerEvent("OutgoingPrivateChatNotification", (user, line))
+
+    def OutgoingPublicChatEvent(self, room, line):
+        return self.TriggerEvent("OutgoingPublicChatEvent", (room, line))
+
+    def OutgoingPublicChatNotification(self, room, line):
+        self.TriggerEvent("OutgoingPublicChatNotification", (room, line))
+
+    def OutgoingGlobalSearchEvent(self, text):
+        return self.TriggerEvent("OutgoingGlobalSearchEvent", (text,))
+
+    def OutgoingRoomSearchEvent(self, rooms, text):
+        return self.TriggerEvent("OutgoingRoomSearchEvent", (rooms, text))
+
+    def OutgoingBuddySearchEvent(self, text):
+        return self.TriggerEvent("OutgoingBuddySearchEvent", (text,))
+
+    def OutgoingUserSearchEvent(self, users):
+        return self.TriggerEvent("OutgoingUserSearchEvent", (users,))
+
+    def UserResolveNotification(self, user, ip, port, country=None):
+        """Notification for user IP:Port resolving.
+
+        Note that country is only set when the user requested the resolving"""
+        self.TriggerEvent("UserResolveNotification", (user, ip, port, country))
+
+    def ServerConnectNotification(self):
+        self.TriggerEvent("ServerConnectNotification", (),)
+
+    def ServerDisconnectNotification(self, userchoice):
+        self.TriggerEvent("ServerDisconnectNotification", (userchoice, ))
+
+    def JoinChatroomNotification(self, room):
+        self.TriggerEvent("JoinChatroomNotification", (room,))
+
+    def LeaveChatroomNotification(self, room):
+        self.TriggerEvent("LeaveChatroomNotification", (room,))
+
+    def UploadQueuedNotification(self, user, virtualfile, realfile):
+        self.TriggerEvent("UploadQueuedNotification", (user, virtualfile, realfile))
+
+    def UserStatsNotification(self, user, stats):
+        self.TriggerEvent("UserStatsNotification", (user, stats))
+
+    # other functions
+    def appendqueue(self, item):
+        # We cannot do a test after adding the item since it's possible
+        # this function will be called twice simultaneously - and then
         # len(self.guiqueue) might be 2 for both calls.
         # Calling the processQueue twice is not a problem though.
         addidle = False
