37b95d81072f9cfc3ad315e735eb68b440af6d2e
Mat <mail@mathias.is>
2021-05-09 08:11:19 +0300

    Update dependencies-core.sh

--- packaging/macos/dependencies-core.sh
+++ packaging/macos/dependencies-core.sh
@@ -28,10 +28,10 @@ brew install -f adwaita-icon-theme \
   flake8 \
   gtk+3
 
-brew uninstall --ignore-dependencies gtk+3
-brew uninstall --ignore-dependencies glib
-brew uninstall --ignore-dependencies gdk-pixbuf
-brew uninstall --ignore-dependencies pango
+#brew uninstall --ignore-dependencies gtk+3
+#brew uninstall --ignore-dependencies glib
+#brew uninstall --ignore-dependencies gdk-pixbuf
+#brew uninstall --ignore-dependencies pango
 brew uninstall --ignore-dependencies python3
 ls
 rm -rf "$(brew --cache)"
@@ -39,22 +39,22 @@ ls
 #brew cleanup -s glib
 #brew cleanup -s python3
 
-curl -L https://bintray.com/homebrew/bottles/download_file?file_path=gdk-pixbuf-2.42.6.mojave.bottle.tar.gz -o gdk-pixbuf-2.42.6.mojave.bottle.tar.gz
-curl -L https://bintray.com/homebrew/bottles/download_file?file_path=glib-2.68.0.mojave.bottle.tar.gz -o glib-2.68.0.mojave.bottle.tar.gz
-curl -L https://bintray.com/homebrew/bottles/download_file?file_path=gtk%2B3-3.24.28.mojave.bottle.tar.gz -o gtk+3-3.24.28.mojave.bottle.tar.gz
-curl -L https://bintray.com/homebrew/bottles/download_file?file_path=pango-1.48.4.mojave.bottle.tar.gz -o pango-1.48.4.mojave.bottle.tar.gz
+#curl -L https://bintray.com/homebrew/bottles/download_file?file_path=gdk-pixbuf-2.42.6.mojave.bottle.tar.gz -o gdk-pixbuf-2.42.6.mojave.bottle.tar.gz
+#curl -L https://bintray.com/homebrew/bottles/download_file?file_path=glib-2.68.0.mojave.bottle.tar.gz -o glib-2.68.0.mojave.bottle.tar.gz
+#curl -L https://bintray.com/homebrew/bottles/download_file?file_path=gtk%2B3-3.24.28.mojave.bottle.tar.gz -o gtk+3-3.24.28.mojave.bottle.tar.gz
+#curl -L https://bintray.com/homebrew/bottles/download_file?file_path=pango-1.48.4.mojave.bottle.tar.gz -o pango-1.48.4.mojave.bottle.tar.gz
 curl -L https://bintray.com/homebrew/bottles/download_file?file_path=python%403.9-3.9.4.mojave.bottle.tar.gz -o python@3.9-3.9.4.mojave.bottle.tar.gz
 ls
 brew install -f --ignore-dependencies python@3.9-3.9.4.mojave.bottle.tar.gz
-brew install -f --ignore-dependencies glib-2.68.0.mojave.bottle.tar.gz
-brew install -f --ignore-dependencies gdk-pixbuf-2.42.6.mojave.bottle.tar.gz
-brew install -f --ignore-dependencies pango-1.48.4.mojave.bottle.tar.gz
-brew install -f --ignore-dependencies gtk+3-3.24.28.mojave.bottle.tar.gz
+#brew install -f --ignore-dependencies glib-2.68.0.mojave.bottle.tar.gz
+#brew install -f --ignore-dependencies gdk-pixbuf-2.42.6.mojave.bottle.tar.gz
+#brew install -f --ignore-dependencies pango-1.48.4.mojave.bottle.tar.gz
+#brew install -f --ignore-dependencies gtk+3-3.24.28.mojave.bottle.tar.gz
 ls
-brew unlink gdk-pixbuf && brew link --force --overwrite gdk-pixbuf
-brew unlink glib && brew link --force --overwrite glib
-brew unlink gtk+3 && brew link --force --overwrite gtk+3
-brew unlink pango && brew link --force --overwrite pango
+#brew unlink gdk-pixbuf && brew link --force --overwrite gdk-pixbuf
+#brew unlink glib && brew link --force --overwrite glib
+#brew unlink gtk+3 && brew link --force --overwrite gtk+3
+#brew unlink pango && brew link --force --overwrite pango
 brew unlink python3 && brew link --force --overwrite python3
 
 # Install dependencies with pip
