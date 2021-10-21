36b1b99844417dcaf2ed502e93a3cf5732ed94f4
mathiascode <mail@mathias.is>
2021-10-21 13:04:33 +0300

    go

--- packaging/windows/setup.py
+++ packaging/windows/setup.py
@@ -4,44 +4,15 @@ from cx_Freeze import setup, Executable
 
 
 common_include_files = []
-required_dll_search_paths = os.getenv("PATH", os.defpath).split(os.pathsep)
-required_dlls = [
-    'libgtk-3-0.dll',
-    'libgdk-3-0.dll',
-    'libepoxy-0.dll',
-    'libgdk_pixbuf-2.0-0.dll',
-    'libpango-1.0-0.dll',
-    'libpangocairo-1.0-0.dll',
-    'libpangoft2-1.0-0.dll',
-    'libpangowin32-1.0-0.dll',
-    'libatk-1.0-0.dll',
-]
-
-for dll in required_dlls:
-    dll_path = None
-    for p in required_dll_search_paths:
-        p = os.path.join(p, dll)
-        if os.path.isfile(p):
-            dll_path = p
-            break
-    assert dll_path is not None, \
-        "Unable to locate {} in {}".format(
-            dll,
-            "",
-        )
-    common_include_files.append((dll_path, dll))
 
 required_gi_namespaces = [
     "Gtk-3.0",
     "Gdk-3.0",
-    "cairo-1.0",
     "Pango-1.0",
     "GObject-2.0",
     "GLib-2.0",
     "Gio-2.0",
     "GdkPixbuf-2.0",
-    "GModule-2.0",
-    "Atk-1.0",
 ]
 
 for ns in required_gi_namespaces:
