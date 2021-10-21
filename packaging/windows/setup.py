d57a87e13a4cc4bc35def8b3142c983ae1c440fa
mathiascode <mail@mathias.is>
2021-10-21 13:40:07 +0300

    hi

--- packaging/windows/setup.py
+++ packaging/windows/setup.py
@@ -1,27 +1,51 @@
 import os
 import sys
-from cx_Freeze import setup, Executable
+from cx_Freeze import Executable, setup
 
 
 common_include_files = []
 
-required_gi_namespaces = [
-    "Gtk-3.0",
-    "Gdk-3.0",
-    "Pango-1.0",
-    "GObject-2.0",
-    "GLib-2.0",
-    "Gio-2.0",
-    "GdkPixbuf-2.0",
+required_dll_search_paths = os.getenv("PATH", os.defpath).split(os.pathsep)
+required_dlls = [
+    'libgtk-3-0.dll',
+    'libgdk-3-0.dll',
+    'libepoxy-0.dll',
+    'libgdk_pixbuf-2.0-0.dll',
+    'libpango-1.0-0.dll',
+    'libpangocairo-1.0-0.dll',
+    'libpangoft2-1.0-0.dll',
+    'libpangowin32-1.0-0.dll',
+    'libatk-1.0-0.dll',
 ]
 
-for ns in required_gi_namespaces:
-    subpath = "lib/girepository-1.0/{}.typelib".format(ns)
+for dll in required_dlls:
+    dll_path = None
+
+    for p in required_dll_search_paths:
+        p = os.path.join(p, dll)
+        if os.path.isfile(p):
+            dll_path = p
+            break
+
+    common_include_files.append((dll_path, dll))
+
+required_gi_namespaces = [
+    "Gtk-3.0",
+    "Gio-2.0",
+    "Gdk-3.0",
+    "GLib-2.0",
+    "HarfBuzz-0.0",
+    "Atk-1.0",
+    "Pango-1.0",
+    "GObject-2.0",
+    "GdkPixbuf-2.0",
+    "cairo-1.0",
+    "GModule-2.0",
+]
+
+for namespace in required_gi_namespaces:
+    subpath = "lib/girepository-1.0/%s.typelib" % namespace
     fullpath = os.path.join(sys.prefix, subpath)
-    assert os.path.isfile(fullpath), (
-        "Required file {} is missing" .format(
-            fullpath,
-        ))
     common_include_files.append((fullpath, subpath))
 
 setup(
