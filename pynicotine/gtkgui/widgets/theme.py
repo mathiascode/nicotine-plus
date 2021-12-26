96819e610f40f3422553bc0eee9d0ae74c282e1e
mathiascode <mail@mathias.is>
2021-12-26 21:56:03 +0200

    Freedesktop appearance

--- pynicotine/gtkgui/widgets/theme.py
+++ pynicotine/gtkgui/widgets/theme.py
@@ -21,6 +21,7 @@ import sys
 
 from gi.repository import Gdk
 from gi.repository import Gio
+from gi.repository import GLib
 from gi.repository import Gtk
 from gi.repository import Pango
 
@@ -32,10 +33,73 @@ from pynicotine.logfacility import log
 """ Global Style """
 
 
+try:
+    SETTINGS_PORTAL = Gio.DBusProxy.new_for_bus_sync(Gio.BusType.SESSION,
+                                                     Gio.DBusProxyFlags.NONE,
+                                                     None,
+                                                     "org.freedesktop.portal.Desktop",
+                                                     "/org/freedesktop/portal/desktop",
+                                                     "org.freedesktop.portal.Settings",
+                                                     None)
+except Exception:
+    pass
+
 GTK_SETTINGS = Gtk.Settings.get_default()
 
 
-def set_dark_mode(enabled):
+def read_color_scheme():
+
+    try:
+        value = SETTINGS_PORTAL.call_sync("Read",
+                                          GLib.Variant("(ss)",
+                                              ("org.freedesktop.appearance",
+                                               "color-scheme")),
+                                          Gio.DBusCallFlags.NONE,
+                                          -1,
+                                          None)
+
+        return value.get_child_value(0).get_variant().get_variant().get_uint32()
+
+    except Exception:
+        return None
+
+
+def on_color_scheme_changed(proxy, sender_name, signal_name, parameters):
+
+    if signal_name != "SettingChanged":
+        return
+
+    if config.sections["ui"]["dark_mode"]:
+        return
+
+    namespace = parameters.get_child_value(0).get_string()
+    name = parameters.get_child_value(1).get_string()
+
+    if namespace != "org.freedesktop.appearance" or name != "color-scheme":
+        return
+
+    color_scheme = parameters.get_child_value(2).get_variant().get_uint32()
+    set_dark_mode(bool(color_scheme), read_system_preference=False)
+
+
+def initialize_portal():
+
+    color_scheme = read_color_scheme()
+
+    if color_scheme is None:
+        return
+
+    SETTINGS_PORTAL.connect("g-signal", on_color_scheme_changed)
+
+    if not config.sections["ui"]["dark_mode"]:
+        set_dark_mode(bool(color_scheme), read_system_preference=False)
+
+
+def set_dark_mode(enabled, read_system_preference=True):
+
+    if read_system_preference and not enabled:
+        enabled = bool(read_color_scheme())
+
     GTK_SETTINGS.set_property("gtk-application-prefer-dark-theme", enabled)
 
 
@@ -189,6 +253,7 @@ def set_global_css():
 
 
 def set_global_style():
+    initialize_portal()
     set_visual_settings()
     set_global_css()
 
