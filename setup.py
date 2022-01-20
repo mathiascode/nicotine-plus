1047652b7da70bf64e3842aebe1b8ff3816497ab
mathiascode <mail@mathias.is>
2022-01-20 20:39:28 +0200

    test

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ functionality while keeping current with the Soulseek protocol."""
         ("share/icons/hicolor/symbolic/apps", glob.glob("pynicotine/gtkgui/icons/hicolor/symbolic/apps/*.svg")),
         ("share/doc/nicotine", glob.glob("[!404.md]*.md") + glob.glob("doc/*.md")),
         ("share/man/man1", glob.glob("data/*.1"))
-    ] + get_translation_paths()
+    ]
 
     # Run setup
     setup(
