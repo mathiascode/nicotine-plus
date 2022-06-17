7e98c7936699de4381b5f08d83655588b7fafecf
Mat <mail@mathias.is>
2022-06-17 09:06:43 +0300

    Update dependencies_packaging.py

--- packaging/windows/dependencies_packaging.py
+++ packaging/windows/dependencies_packaging.py
@@ -29,10 +29,10 @@ ARCH = os.environ.get("ARCH") or "x86_64"
 def install_pacman():
     """ Install dependencies from the main MinGW repos """
 
-    prefix = "mingw-w64-" + ARCH + "-"
-    packages = [prefix + "python-cx-freeze"]
+    prefix = "mingw64/mingw-w64-x86_64" if ARCH == "x86_64" else "mingw32/mingw-w64-i386"
+    packages = ["https://repo.msys2.org/mingw/%s-python-cx-freeze-6.10-1-any.pkg.tar.zst" % prefix]
 
-    subprocess.check_call(["pacman", "--noconfirm", "-S", "--needed"] + packages)
+    subprocess.check_call(["pacman", "--noconfirm", "-U"] + packages)
 
 
 if __name__ == '__main__':
