99fb651737c20eb3ab2b48ba67de1d2b5eca6691
Mat <mail@mathias.is>
2022-08-17 17:06:27 +0300

    Update __init__.py

--- pynicotine/gtkgui/__init__.py
+++ pynicotine/gtkgui/__init__.py
@@ -112,9 +112,5 @@ def run_gui(core, hidden, ci_mode, multi_instance):
 
     from gi.repository import Gdk
 
-    if Gdk.Display.get_default() is None:
-        log.add(_("No graphical environment available, using headless (no GUI) mode"))
-        return None
-
     from pynicotine.gtkgui.application import Application
     return Application(core, hidden, ci_mode, multi_instance).run()
