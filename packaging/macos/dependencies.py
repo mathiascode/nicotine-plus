0563a01b0222f94ab57b04892277b4e33b03df84
Mat <mail@mathias.is>
2024-01-28 03:54:18 +0200

    Update dependencies.py

--- packaging/macos/dependencies.py
+++ packaging/macos/dependencies.py
@@ -45,7 +45,7 @@ def install_brew():
 def install_pypi():
     """Install dependencies from PyPi."""
 
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "--no-binary", ":all:",
+    subprocess.check_call([sys.executable, "-m", "pip", "install",
                            "-e", ".[packaging,tests]", "build"])
 
 
