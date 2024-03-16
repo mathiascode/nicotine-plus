5d3b56408a155d8a6f12360a73575286b8c31b16
Mat <mail@mathias.is>
2024-03-16 19:38:43 +0200

    Update dependencies.py

--- packaging/macos/dependencies.py
+++ packaging/macos/dependencies.py
@@ -46,8 +46,7 @@ def install_pypi():
     """Install dependencies from PyPi."""
 
     subprocess.check_call([sys.executable, "-m", "pip", "install", "--no-binary", ":all:",
-                           "-e", ".[packaging,tests]", "build",
-                           "git+https://github.com/marcelotduarte/cx_Freeze"])
+                           "-e", ".[packaging,tests]", "build"])
 
 
 if __name__ == "__main__":
